99a868cc6519428cf0e177714b485c8bb7886057
José Reinoza <josemrd1994@gmail.com>
2021-06-11 17:30:13 -0400

    Little CSS fix

--- client/src/components/shared/Navbar.js
+++ client/src/components/shared/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Layout } from 'antd';
 import { BellOutlined, MenuOutlined } from '@ant-design/icons';
-import '../../assets/css/navbar.css';
+import '../../assets/css/Navbar.css';
 import userpic from '../../assets/img/user-icon-image-13.jpg';
 
 const { Header } = Layout;
