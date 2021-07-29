10631a4de0a9df4c543ff8570a1efafe1ff7c19e
Anthony Briceño <anthony.briceno@mykukun.com>
2021-07-29 18:42:45 -0400

    Improved responsive dashboard with header, sidebar, content area, footer

---
+++ client/src/pages/Dashboard2.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Col, Row, Layout } from 'antd';
+import Navbar from '../components/shared/Navbar';
+import Sidebar from '../components/shared/Sidebar';
+import NavFooter from '../components/shared/footer';
+import ContentDash from '../components/shared/ContentDash';
+
+
+function Dashboard2(props) {
+
+  return (
+   <>
+      <Layout>
+        <Sidebar />
+        <Layout style={{ marginLeft: 200 }}>
+          <Navbar isLogged={false}/>
+          <ContentDash />
+          <NavFooter />
+        </Layout>
+      </Layout>
+   </>
+  );
+}
+
+export default Dashboard2;
