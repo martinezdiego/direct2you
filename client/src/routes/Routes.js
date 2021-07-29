5eec85ae8548cef494a550d2ffaa08d836099e52
Anthony Briceño <anthony.briceno@mykukun.com>
2021-07-29 18:47:34 -0400

    Changed view Dashboard2

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switc, Route, Switch } from 'react-router-dom';
 
 //Components
-import Dashboard from '../pages/Dashboard';
+import Dashboard2 from '../pages/Dashboard2';
 
 function Routes() {
 
   return (
    <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={ Dashboard }/>
+      <Route exact path="/" component={ Dashboard2 }/>
     </Switch>
    </BrowserRouter>
   );
