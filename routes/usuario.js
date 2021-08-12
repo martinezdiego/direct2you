e22d5c77c2356608ccd325c6cdd2cc8415e1f56f
Diego Martinez <diegoalejandromartinezrivas@gmail.com>
2021-08-11 23:03:51 -0400

    updating user routes

--- routes/usuario.js
+++ routes/usuario.js
@@ -2,8 +2,16 @@ const usuario = require('../controllers/usuario');
 
 const router = require('express').Router();
 
-router.post("/", usuario.create);
+router.post("/crear", usuario.create);
 
-router.get("/", usuario.findAll);
+router.get("/todos", usuario.findAll);
+
+router.get("/:id", usuario.findOne);
+
+router.put("/:id", usuario.update);
+
+router.put("/:id/eliminar", usuario.delete);
+
+router.delete("/todos/eliminar", usuario.deleteAll);
 
 module.exports = router;
