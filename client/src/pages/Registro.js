38e18b6a288b13ed9052986ec163df034fe58143
Anthony Briceño <anthony.briceno@mykukun.com>
2021-08-16 14:51:19 -0400

    Register Page

---
+++ client/src/pages/Registro.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Layout } from 'antd';
+import Navbar from '../components/shared/Navbar';
+import NavFooter from '../components/shared/footer';
+import ContentRegistro from '../components/shared/ContentRegistro';
+
+
+function Registro(props) {
+
+  return (
+   <>
+      <Layout>
+        <Navbar isLogged={false}/>
+        <ContentRegistro />
+        <NavFooter />
+      </Layout>
+   </>
+  );
+}
+
+export default Registro;
