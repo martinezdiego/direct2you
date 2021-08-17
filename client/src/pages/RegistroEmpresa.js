397332cf57d39215fd270ca0bdfb462c82d2623e
Anthony2696 <the.chee.21@gmail.com>
2021-08-17 15:40:50 -0400

    Create new page Register Empresa

---
+++ client/src/pages/RegistroEmpresa.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Layout } from 'antd';
+import Navbar from '../components/shared/Navbar';
+import NavFooter from '../components/shared/footer';
+import ContentRegistroEmpresa from '../components/shared/ContentRegistroEmpresa';
+
+
+function RegistroEmpresa(props) {
+
+  return (
+   <>
+      <Layout>
+        <Navbar isLogged={false}/>
+        <ContentRegistroEmpresa />
+        <NavFooter />
+      </Layout>
+   </>
+  );
+}
+
+export default RegistroEmpresa;
