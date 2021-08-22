cf4a07bf1e8bff55f9a873f39f4a116300e042c4
Diego Martinez <diegoalejandromartinezrivas@gmail.com>
2021-08-22 11:27:02 -0400

    add validation to user type routes

--- routes/tipoUsuario.js
+++ routes/tipoUsuario.js
@@ -2,10 +2,16 @@ const tipoUsuario = require('../controllers/tipoUsuario');
 
 const router = require('express').Router();
 
-router.post("/", tipoUsuario.create);
+router.post("/crear", tipoUsuario.create);
 
 router.get("/", tipoUsuario.findAll);
 
 router.get("/:id", tipoUsuario.findOne);
+
+router.put("/:id", tipoUsuario.update);
+
+router.delete("/:id", tipoUsuario.delete);
+
+router.delete("/", tipoUsuario.deleteAll);
 
 module.exports = router;
