6a634c89a6ff40a968b750cdf5e06fe757ff7d45
Anthony2696 <the.chee.21@gmail.com>
2021-08-22 13:12:50 -0400

    Component Changed from funtion to class

--- client/src/components/shared/ContentRegistro.jsx
+++ client/src/components/shared/ContentRegistro.jsx
@@ -8,71 +8,72 @@ import '../../assets/css/Navfooter.css';
 
 const { Content } = Layout;
 
-function ContentRegistro(props) {
+class ContentRegistro extends React.Component {
+  render(){
+    return(
+      <Content style={{ padding: '0 50px', marginTop: 64 }}>
+        <div style={{textAlign:'center'}}>
+          <h1 className="h1-reg">Registro</h1>
+        </div>
+        <div className="Col-left">
 
-  return(
-    <Content style={{ padding: '0 50px', marginTop: 64 }}>
-      <div style={{textAlign:'center'}}>
-        <h1 className="h1-reg">Registro</h1>
-      </div>
-      <div className="Col-left">
+            <Divider>
+              <FileDoneOutlined className="icon"/>
+            </Divider>
+            <Row>
+              <Col flex={2}>
+                <div style={{float:'right', marginRight:'10px',marginTop:'10px'}}>
+                  <Badge count={1} style={{background:'green',color:'white',borderColor:'green'}} />
+                </div>
+              </Col>
+              <Col flex={2}>
+                <p className="p-left">Completa el formulario de registro</p>
+              </Col>
+            </Row>
 
           <Divider>
-            <FileDoneOutlined className="icon"/>
+            <ShoppingCartOutlined className="icon"/>
           </Divider>
           <Row>
             <Col flex={2}>
               <div style={{float:'right', marginRight:'10px',marginTop:'10px'}}>
-                <Badge count={1} style={{background:'green',color:'white',borderColor:'green'}} />
+                <Badge count={2} style={{background:'green',color:'white',borderColor:'green'}} />
               </div>
             </Col>
-            <Col flex={2}>
-              <p className="p-left">Completa el formulario de registro</p>
-            </Col>
+            <Col flex={2}><p className="p-left">Escoge los productos que desees y has el pedido</p></Col>
           </Row>
-
-        <Divider>
-          <ShoppingCartOutlined className="icon"/>
-        </Divider>
-        <Row>
-          <Col flex={2}>
-            <div style={{float:'right', marginRight:'10px',marginTop:'10px'}}>
-              <Badge count={2} style={{background:'green',color:'white',borderColor:'green'}} />
-            </div>
-          </Col>
-          <Col flex={2}><p className="p-left">Escoge los productos que desees y has el pedido</p></Col>
-        </Row>
-        <Divider>
-          <DollarOutlined className="icon"/>
-        </Divider>
-        <Row>
-          <Col flex={2}>
-            <div style={{float:'right', marginRight:'10px',marginTop:'10px'}}>
-              <Badge count={3} style={{background:'green',color:'white',borderColor:'green'}} />
-            </div>
-          </Col>
-          <Col flex={2}><p className="p-left">Realiza el pago de los productos</p></Col>
-        </Row>
-        <Divider>
-          <LikeOutlined className="icon"/>
-        </Divider>
-        <Row>
-          <Col flex={2}>
-            <div style={{float:'right', marginRight:'10px',marginTop:'10px'}}>
-              <Badge count={4} style={{background:'green',color:'white',borderColor:'green'}} />
-            </div>
-          </Col>
-          <Col flex={2}><p className="p-left">Recibe el pedido</p></Col>
-        </Row>
-      </div>
-      
-      <div className="Col-right">
-        <div style={{justifyContent:'center',textAlign:'center'}}>
-          <RegistrationForm />
+          <Divider>
+            <DollarOutlined className="icon"/>
+          </Divider>
+          <Row>
+            <Col flex={2}>
+              <div style={{float:'right', marginRight:'10px',marginTop:'10px'}}>
+                <Badge count={3} style={{background:'green',color:'white',borderColor:'green'}} />
+              </div>
+            </Col>
+            <Col flex={2}><p className="p-left">Realiza el pago de los productos</p></Col>
+          </Row>
+          <Divider>
+            <LikeOutlined className="icon"/>
+          </Divider>
+          <Row>
+            <Col flex={2}>
+              <div style={{float:'right', marginRight:'10px',marginTop:'10px'}}>
+                <Badge count={4} style={{background:'green',color:'white',borderColor:'green'}} />
+              </div>
+            </Col>
+            <Col flex={2}><p className="p-left">Recibe el pedido</p></Col>
+          </Row>
         </div>
-      </div>
-    </Content>
-  );
+        
+        <div className="Col-right">
+          <div style={{justifyContent:'center',textAlign:'center'}}>
+            <RegistrationForm />
+          </div>
+        </div>
+      </Content>
+    )
+  };
 }
 
 export default ContentRegistro;
