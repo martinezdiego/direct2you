0b0f73980de91c4666674733d11272e48a79dd15
Anthony2696 <the.chee.21@gmail.com>
2021-08-22 12:06:02 -0400

    Component Changed from funtion to class

--- client/src/components/shared/footer.jsx
+++ client/src/components/shared/footer.jsx
@@ -7,43 +7,45 @@ import '../../assets/css/Navfooter.css';
 
 const { Footer } = Layout;
 
-function NavFooter(props) {
-    return(
-        <Footer style={{background:"#ffffff"}}>
-            <div className="Container">
-                <Row className="Row">
-                    <Col className="Column" >
-                        <div style={{textAlign:'center'}}>
-                            <Image
-                                width={150}
-                                preview={false}
-                                src={logo}
-                            />
-                        </div>
-                    </Col>
-                    <Col className="Column" >
-                        <div className="Heading">Contacto</div>
-                        <div>contac@direct2you.com</div>
-                        <div>+58 0412-1234567</div>
-                    </Col>
-                    <Col className="Column" >
-                        <div className="Heading">Direccion</div>
-                        <div>Calle #, entre avenidas # y # Local #</div>
-                    </Col>
-                    <Col className="Column" >
-                        <div className="Heading">Encuentranos en</div>
-                        <div style={{textAlign:'center'}}>
-                            <Space>
-                                <Button className="ButtonFooter" shape="circle" href="www.instagram.com/direct2you" icon={<InstagramOutlined />} />
-                                <Button className="ButtonFooter" shape="circle" href="www.facebook.com/direct2you" icon={<FacebookOutlined />} />
-                                <Button className="ButtonFooter" shape="circle" href="@direct2you" icon={<WhatsAppOutlined />} />
-                            </Space>
-                        </div>
-                    </Col>
-                </Row>
-            </div>
-        </Footer>
-    );
+class NavFooter extends React.Component {
+    render(){
+        return(
+            <Footer style={{background:"#ffffff"}}>
+                <div className="Container">
+                    <Row className="Row">
+                        <Col className="Column" >
+                            <div style={{textAlign:'center'}}>
+                                <Image
+                                    width={150}
+                                    preview={false}
+                                    src={logo}
+                                />
+                            </div>
+                        </Col>
+                        <Col className="Column" >
+                            <div className="Heading">Contacto</div>
+                            <div>contac@direct2you.com</div>
+                            <div>+58 0412-1234567</div>
+                        </Col>
+                        <Col className="Column" >
+                            <div className="Heading">Direccion</div>
+                            <div>Calle #, entre avenidas # y # Local #</div>
+                        </Col>
+                        <Col className="Column" >
+                            <div className="Heading">Encuentranos en</div>
+                            <div style={{textAlign:'center'}}>
+                                <Space>
+                                    <Button className="ButtonFooter" shape="circle" href="www.instagram.com/direct2you" icon={<InstagramOutlined />} />
+                                    <Button className="ButtonFooter" shape="circle" href="www.facebook.com/direct2you" icon={<FacebookOutlined />} />
+                                    <Button className="ButtonFooter" shape="circle" href="@direct2you" icon={<WhatsAppOutlined />} />
+                                </Space>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+            </Footer>
+        )
+    };
 }
 
 export default NavFooter;
