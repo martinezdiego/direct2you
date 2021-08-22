5682c6b141530383ea02003d49635a1d4cd14463
Anthony2696 <the.chee.21@gmail.com>
2021-08-22 12:01:06 -0400

    Fixed Navbar Responsive for screen < 445px

--- client/src/components/shared/Navbar.jsx
+++ client/src/components/shared/Navbar.jsx
@@ -1,45 +1,95 @@
 import React from 'react';
-import { Layout, Button, Space } from 'antd';
-import { BellOutlined, MenuOutlined, HomeOutlined } from '@ant-design/icons';
+import { Layout, Button, Space,Menu, Dropdown } from 'antd';
+import { BellOutlined, MenuOutlined, HomeOutlined,UserOutlined } from '@ant-design/icons';
 import '../../assets/css/Navbar.css';
 import userpic from '../../assets/img/user-icon-image-13.jpg';
 
 const { Header } = Layout;
 
-function Navbar(props) {
-    const isLogged = props.isLogged;
-    if (isLogged){
-        return(
-            <Header className="navbar">
-                <ul className="nav-ul-left">
-                    <li><MenuOutlined className="menu-icon"/></li>
-                    <li className="nav-title">Direct2You</li>
-                </ul>
-                <ul className="nav-ul-right">
-                    <li><BellOutlined /></li>
-                    <li><img src={userpic} className="nav-user-img" alt=''></img></li>
-                </ul>
-            </Header>
-        );
+class Navbar extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { hidenav: false };
+      }
+      
+    componentDidMount() {
+        window.addEventListener("resize", this.resize.bind(this));
+        this.resize();
     }
-    return(
-        <Header className="navbar">
-            <ul className="nav-ul-left">
-                <li><MenuOutlined className="menu-icon"/></li>
-                <li className="nav-title">Direct2You</li>
-            </ul>
-            <ul className="nav-ul-right">
-                <Space>
-                    <Button href="#registro" type="default" icon={<HomeOutlined />}>
-                        Afiliarme
-                    </Button>
-                    <Button href="#login" type="default" >
-                        Ingresar
-                    </Button>
-                </Space>
-            </ul>
-        </Header>
-    );
+    
+    resize() {
+        let currentHideNav = (window.innerWidth <= 445);
+        if (currentHideNav !== this.state.hideNav) {
+            this.setState({hideNav: currentHideNav});
+        }
+    }
+    
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.resize.bind(this));
+    }
+
+    render(){        
+        const isLogged = this.props.isLogged;
+        const menu = (
+            <Menu >
+                <Menu.Item key="1" icon={<HomeOutlined />}>
+                    <a href='#IR A REGISTER PAGE'>Afiliarme</a>
+                </Menu.Item>
+                <Menu.Item key="2" icon={<UserOutlined />}>
+                <a href='#IR A LOGIN PAGE'>Ingresar</a>
+                </Menu.Item>
+            </Menu>
+        );   
+    
+        if (isLogged){
+            return(
+                <Header className="navbar">
+                    <ul className="nav-ul-left">
+                        <li><MenuOutlined className="menu-icon"/></li>
+                        <li className="nav-title">Direct2You</li>
+                    </ul>
+                    <ul className="nav-ul-right">
+                        <li><BellOutlined /></li>
+                        <li><img src={userpic} className="nav-user-img" alt=''></img></li>
+                    </ul>
+                </Header>
+            )
+        }else if (this.state.hideNav){
+            return(
+                <Header className="navbar">
+                    <ul className="nav-ul-left">
+                        <li>
+                            <Dropdown overlay={menu}>
+                                <li><MenuOutlined className="menu-icon"/></li>
+                            </Dropdown>
+                        </li>
+
+                        <li className="nav-title">Direct2You</li>
+                    </ul>
+                </Header>
+            )
+        }else{
+            return(
+                <Header className="navbar">
+                    <ul className="nav-ul-left">
+                        <li><MenuOutlined className="menu-icon"/></li>
+                        <li className="nav-title">Direct2You</li>
+                    </ul>
+                    <ul className="nav-ul-right">
+                        <Space>
+                            <Button href="#registro" type="default" icon={<HomeOutlined />}>
+                                Afiliarme
+                            </Button>
+                            <Button href="#login" type="default" >
+                                Ingresar
+                            </Button>
+                        </Space>
+                    </ul>
+                </Header>
+            )
+        }
+    };
 }
 
 export default Navbar;
