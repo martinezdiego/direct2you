b2c50845e34e04dc3f9990c0e2362d6137405109
Diego Martinez <diegoalejandromartinezrivas@gmail.com>
2021-08-28 13:36:27 -0400

    Add passport authentication

--- app.js
+++ app.js
@@ -1,21 +1,71 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
 
 const settings = require('./settings');
 
 const models = require('./models');
 
+// Routers
 const usuario = require('./routes/usuario');
 const tipoUsuario = require('./routes/tipoUsuario');
 const empresa = require('./routes/empresa');
 const categoriaEmpresa = require('./routes/categoriaEmpresa');
 
+// App config
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
+// Database connection
+models.sequelize.sync();
+
+// Passport setup
+passport.use(new Strategy({
+        usernameField: 'correo_usuario',
+        passwordField: 'contrasena'
+    },
+    async (email, password, done) => {
+        try {
+            const response = await models.Usuario.findOne({ 
+                where: { 
+                    correo_usuario: email   
+                } 
+            });
+            if (!response) {
+                return done(null, false, { message: 'Incorrect email' });
+            }
+            if (response.contrasena != password) {
+                return done(null, false, { message: 'Incorrect password' });
+            }
+            return done(null, response);
+        }
+        catch (err) {
+            return done(err);
+        }   
+    }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id_usuario);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const response = await models.Usuario.findByPk(id);
+        done(null, response);
+    }
+    catch (err) {
+        done(err);
+    }
+});
+
+// Routes
 app.use("/api/usuarios", usuario);
 app.use("/api/tipo_usuarios", tipoUsuario);
 app.use("/api/empresas", empresa);
@@ -35,6 +85,5 @@ if (settings.env === 'production') {
   });
 }
 
-models.sequelize.sync();
-
+// Listen on provided port
 app.listen(settings.port, () => console.log(`Listening on port ${settings.port}`)); 
