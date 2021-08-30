c37b93d425b3c9c6cc8476be2453ce7174670e5a
Diego Martinez <diegoalejandromartinezrivas@gmail.com>
2021-08-29 21:11:43 -0400

    Add validation to city routes

--- controllers/ciudad.js
+++ controllers/ciudad.js
@@ -1,22 +1,46 @@
+const { body, param, validationResult } = require('express-validator');
+
 const { Ciudad } = require('../models');
 
-exports.create = async (req, res) => {
-    const { body } = req;
+exports.create = [
+    body('nombre_ciudad')
+        .exists()
+        .withMessage('must be specified')
+        .isLength({ min: 1, max: 255 })
+        .withMessage('must have length more than 0 and less than 256')
+        .matches(/^[a-zA-Z]+$/)
+        .withMessage('must have only letters')
+        .custom(async (value) => {
+            const response = await Ciudad.findOne({ where: { nombre_ciudad: value } });
+            if (response) {
+                throw new Error('city already exists');
+            }
+        }),
+    async (req, res) => {
+        const errors = validationResult(req); 
 
-    const data = {
-        nombre_ciudad: body.nombre_ciudad
-    };
-    
-    try {
-        const response = await Ciudad.create(data);
-        res.send(response);
+        if (!errors.isEmpty()) {
+            res.status(422).json({ errors: errors.array() });
+            return;
+        }
+        
+        const { body } = req;
+
+        const data = {
+            nombre_ciudad: body.nombre_ciudad
+        };
+        
+        try {
+            const response = await Ciudad.create(data);
+            res.send(response);
+        }
+        catch (err) {
+            res.status(500).send({
+                message: err.message || "unexpected error has been occurred when creating city."
+            });
+        }
     }
-    catch (err) {
-        res.status(500).send({
-            message: err.message || "unexpected error has been occurred when creating city."
-        });
-    }
-};
+];
 
 exports.findAll = async (req, res) => {
     try {
@@ -30,58 +54,126 @@ exports.findAll = async (req, res) => {
     }
 };
 
-exports.findOne = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const response = await Ciudad.findByPk(id);
-        res.send(response);
-    }
-    catch (err) {
-        res.status(500).send({
-            message: err.message || `unexpected error has been occurred when retreaving city ${id}.`
-        });
-    }
-};
+exports.findOne = [
+    param('id')
+        .exists()
+        .withMessage('must be specified')
+        .custom(async (value) => {
+            const response = await Ciudad.findByPk(value);
+            if (!response) {
+                throw new Error('invalid city id');
+            }
+        }),
+    async (req, res) => {
+        const errors = validationResult(req); 
 
-exports.update = async (req, res) => {
-    const { id } = req.params;
-    const data  = req.body;
-    
-    try {
-        const response = await Ciudad.findByPk(id);
-        
-        response.nombre_ciudad = data.nombre_ciudad;
-        
-        await response.save();
-        
-        res.send(response);
-    }
-    catch (err) {
-        res.status(500).send({
-            message: err.message || `unexpected error has been occurred when updating city ${id}`
-        });
-    }
-};
-
-exports.delete = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const response = await Ciudad.destroy({
-            where : { id_ciudad : id }
-        });
-        if (response == 1) {
-            res.send({ status: true, message: "city has been deleted" });
+        if (!errors.isEmpty()) {
+            res.status(422).json({ errors: errors.array() });
+            return;
         }
-        else {
-            res.send({ status: false, message: "city has not been deleted" });
+        
+        const { id } = req.params;
+        try {
+            const response = await Ciudad.findByPk(id);
+            res.send(response);
+        }
+        catch (err) {
+            res.status(500).send({
+                message: err.message || `unexpected error has been occurred when retreaving city ${id}.`
+            });
         }
     }
-    catch (err) {
-        res.status(500).send({
-            message: err.message || `unexpected error has been occurred when deleting city ${id}`
-        });
+];
+
+exports.update = [
+    param('id')
+        .exists()
+        .withMessage('must be specified')
+        .custom(async (value) => {
+            const response = await Ciudad.findByPk(value);
+            if (!response) {
+                throw new Error('invalid city id');
+            }
+        }),
+    body('nombre_ciudad')
+        .exists()
+        .withMessage('must be specified')
+        .isLength({ min: 1, max: 255 })
+        .withMessage('must have length more than 0 and less than 256')
+        .matches(/^[a-zA-Z]+$/)
+        .withMessage('must have only letters')
+        .custom(async (value) => {
+            const response = await Ciudad.findOne({ where: { nombre_ciudad: value } });
+            if (response) {
+                throw new Error('city already exists');
+            }
+        }),
+    async (req, res) => {
+        const errors = validationResult(req); 
+
+        if (!errors.isEmpty()) {
+            res.status(422).json({ errors: errors.array() });
+            return;
+        }
+        
+        const { id } = req.params;
+        const data  = req.body;
+        
+        try {
+            const response = await Ciudad.findByPk(id);
+            
+            response.nombre_ciudad = data.nombre_ciudad;
+            
+            await response.save();
+            
+            res.send(response);
+        }
+        catch (err) {
+            res.status(500).send({
+                message: err.message || `unexpected error has been occurred when updating city ${id}`
+            });
+        }
     }
-};
+];
+
+exports.delete = [
+    param('id')
+        .exists()
+        .withMessage('must be specified')
+        .custom(async (value) => {
+            const response = await Ciudad.findByPk(value);
+            if (!response) {
+                throw new Error('invalid city id');
+            }
+        }),
+    async (req, res) => {
+        const errors = validationResult(req); 
+
+        if (!errors.isEmpty()) {
+            res.status(422).json({ errors: errors.array() });
+            return;
+        }
+        
+        const { id } = req.params;
+        
+        try {
+            const response = await Ciudad.destroy({
+                where : { id_ciudad : id }
+            });
+            if (response == 1) {
+                res.send({ status: true, message: "city has been deleted" });
+            }
+            else {
+                res.send({ status: false, message: "city has not been deleted" });
+            }
+        }
+        catch (err) {
+            res.status(500).send({
+                message: err.message || `unexpected error has been occurred when deleting city ${id}`
+            });
+        }
+    }
+];
 
 exports.deleteAll = async (req, res) => {
     try {
