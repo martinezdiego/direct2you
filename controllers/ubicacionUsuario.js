091ff24feaedd9618509813ba2779fa21a0c6cc8
Diego Martinez <diegoalejandromartinezrivas@gmail.com>
2021-09-19 15:17:47 -0400

    Add validation to controllers

--- controllers/ubicacionUsuario.js
+++ controllers/ubicacionUsuario.js
@@ -1,24 +1,54 @@
-const { UbicacionUsuario } = require('../models');
+const { body, param, validationResult } = require('express-validator');
+
+const { UbicacionUsuario, Usuario, Ubicacion } = require('../models');
 
 
-exports.create = async (req, res) => {
-    const { body } = req;
+exports.create = [
+    body("fk_id_usuario")
+    .exists()
+    .withMessage("must be specified")
+    .custom(async (value) => {
+        const response = await Usuario.findByPk(value);
+        if (!response) {
+            throw new Error('invalid id');
+        }
+    }),
+    body("fk_id_ubicacion")
+    .exists()
+    .withMessage("must be specified")
+    .custom(async (value) => {
+        const response = await Ubicacion.findByPk(value);
+        if (!response) {
+            throw new Error('invalid id');
+        }
+    }),
+    async (req, res) => {
+        const errors = validationResult(req); 
 
-    const data = {
-        fk_id_usuario: body.fk_id_usuario,
-        fk_id_ubicacion: body.fk_id_ubicacion
-    };
-    
-    try {
-        const response = await UbicacionUsuario.create(data);
-        res.send(response);
+        if (!errors.isEmpty()) {
+            res.status(422).json({ errors: errors.array() });
+            return;
+        }
+        
+        
+        const { body } = req;
+
+        const data = {
+            fk_id_usuario: body.fk_id_usuario,
+            fk_id_ubicacion: body.fk_id_ubicacion
+        };
+        
+        try {
+            const response = await UbicacionUsuario.create(data);
+            res.send(response);
+        }
+        catch (err) {
+            res.status(500).send({
+                message: err.message || "unexpected error has been occurred when creating entry."
+            });
+        }
     }
-    catch (err) {
-        res.status(500).send({
-            message: err.message || "unexpected error has been occurred when creating entry."
-        });
-    }
-};
+];
 
 exports.findAll = async (req, res) => {
     try {
@@ -32,66 +62,129 @@ exports.findAll = async (req, res) => {
     }
 };
 
-exports.findAllOfUser = async (req, res) => {
-    const { id } = req.params;
-        
-    try {
-        const response = await UbicacionUsuario.findAll({
-            where: { fk_id_usuario : id }
-        });
-        res.send(response);
-    }
-    catch (err) {
-        res.status(500).send({
-            message: err.message || `unexpected error has been occurred when retreaving entries of user ${id}.`
-        });
-    }
-};
-
-exports.deleteOneOfUser = async (req, res) => {
-    const { userId, ubicationId } = req.params;
-        
-    try {
-        const response = await UbicacionUsuario.destroy({
-            where: { 
-                fk_id_usuario : userId,
-                fk_id_ubicacion: ubicationId
-            }
-        });
-         if (response == 1) {
-            res.send({ status: true, message: "ubication has been deleted from user" });
+exports.findAllOfUser = [
+    param("id")
+    .exists()
+    .withMessage("must be specified")
+    .custom(async (value) => {
+        const response = await Usuario.findByPk(value);
+        if (!response) {
+            throw new Error('invalid id');
         }
-        else {
-            res.send({ status: false, message: "ubication has not been deleted from user" });
+    }),
+    async (req, res) => {
+        const errors = validationResult(req); 
+
+        if (!errors.isEmpty()) {
+            res.status(422).json({ errors: errors.array() });
+            return;
+        }
+        
+        const { id } = req.params;
+            
+        try {
+            const response = await UbicacionUsuario.findAll({
+                where: { fk_id_usuario : id }
+            });
+            res.send(response);
+        }
+        catch (err) {
+            res.status(500).send({
+                message: err.message || `unexpected error has been occurred when retreaving entries of user ${id}.`
+            });
         }
     }
-    catch (err) {
-        res.status(500).send({
-            message: err.message || `unexpected error has been occurred when deleting entry of user ${userId} and ubication ${ubicationId}.`
-        });
-    }
-};
+];
 
-exports.deleteAllOfUser = async (req, res) => {
-    const { id } = req.params;
+exports.deleteOneOfUser = [
+    param("userId")
+    .exists()
+    .withMessage("must be specified")
+    .custom(async (value) => {
+        const response = await Usuario.findByPk(value);
+        if (!response) {
+            throw new Error('invalid id');
+        }
+    }),
+    param("ubicationId")
+    .exists()
+    .withMessage("must be specified")
+    .custom(async (value) => {
+        const response = await Ubicacion.findByPk(value);
+        if (!response) {
+            throw new Error('invalid id');
+        }
+    }),
+    async (req, res) => {
+        const errors = validationResult(req); 
+
+        if (!errors.isEmpty()) {
+            res.status(422).json({ errors: errors.array() });
+            return;
+        }
         
-    try {
-        const response = await UbicacionUsuario.destroy({
-            where: { 
-                fk_id_usuario : id,
+        const { userId, ubicationId } = req.params;
+            
+        try {
+            const response = await UbicacionUsuario.destroy({
+                where: { 
+                    fk_id_usuario : userId,
+                    fk_id_ubicacion: ubicationId
+                }
+            });
+            if (response == 1) {
+                res.send({ status: true, message: "ubication has been deleted from user" });
             }
-        });
-         res.send({
-            status: true,
-            message: `${response} entries has been deleted`
-        });
+            else {
+                res.send({ status: false, message: "ubication has not been deleted from user" });
+            }
+        }
+        catch (err) {
+            res.status(500).send({
+                message: err.message || `unexpected error has been occurred when deleting entry of user ${userId} and ubication ${ubicationId}.`
+            });
+        }
     }
-    catch (err) {
-        res.status(500).send({
-            message: err.message || `unexpected error has been occurred when deleting entry of user ${userId} and ubication ${ubicationId}.`
-        });
+];
+
+exports.deleteAllOfUser = [
+    param("id")
+    .exists()
+    .withMessage("must be specified")
+    .custom(async (value) => {
+        const response = await Usuario.findByPk(value);
+        if (!response) {
+            throw new Error('invalid id');
+        }
+    }),
+    async (req, res) => {
+        const errors = validationResult(req); 
+
+        if (!errors.isEmpty()) {
+            res.status(422).json({ errors: errors.array() });
+            return;
+        }
+        
+        const { id } = req.params;
+            
+        try {
+            const response = await UbicacionUsuario.destroy({
+                where: { 
+                    fk_id_usuario : id,
+                }
+            });
+            res.send({
+                status: true,
+                message: `${response} entries has been deleted`
+            });
+        }
+        catch (err) {
+            res.status(500).send({
+                message: err.message || `unexpected error has been occurred when deleting entry of user ${userId} and ubication ${ubicationId}.`
+            });
+        }
     }
-};
+];
 
 exports.deleteAll = async (req, res) => {
     try {
