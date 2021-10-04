bea7351dab582ade554d2a9f1d34bc17758aedc9
Diego Martinez <diegoalejandromartinezrivas@gmail.com>
2021-10-04 17:23:52 -0400

    Add persistence redux state to local storage

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,17 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import throttle from 'lodash/throttle';
 import appReducer from './reducers';
+import { loadState, saveState } from './localStorage';
 
-const store = createStore(appReducer,applyMiddleware(thunk));
+const persistedState = loadState();
+
+const store = createStore(appReducer, persistedState, applyMiddleware(thunk));
+
+store.subscribe(throttle(() => {
+  saveState({
+    session: store.getState().session
+  });
+}, 1000));
 
 export default store;
