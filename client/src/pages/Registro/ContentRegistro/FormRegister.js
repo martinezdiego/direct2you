5b53763698cf8eecbe54b5618efc0f104fb8809d
Diego Martinez <diegoalejandromartinezrivas@gmail.com>
2021-10-04 21:33:53 -0400

    Add handler for sign up

--- client/src/pages/Registro/ContentRegistro/FormRegister.js
+++ client/src/pages/Registro/ContentRegistro/FormRegister.js
@@ -1,6 +1,11 @@
-import React from 'react';
-import { Form, Input, Select, Button } from 'antd';
+import React, { PureComponent } from 'react';
+import { Form, Input, Select, Button, message } from 'antd';
+import { withRouter } from 'react-router-dom';
+
 import AñadirDireccionUSer from "./FormRegister/AñadirDireccionUser"
+
+import { signUp } from '../../../actions/userActions';
+
 const { Option } = Select;
 
 const formItemLayout = {
@@ -41,167 +46,212 @@ const tailFormItemLayout = {
   },
 };
 
-const FormRegister = () => {
-  const [form] = Form.useForm();
+class FormRegister extends PureComponent 
+{
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            isLoading: false
+        };
+        
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+   
+    handleSubmit = async (values) => {
+        const data = {
+            "nombre_usuario": values.name,
+            "apellido": values.lastname,
+            "correo_usuario": values.email,
+            "contrasena": values.password,
+            "num_telefono_usuario": (values.prefix + values.phone),
+            "num_cedula": values.id,
+            "estado_usuario": "habilitado",
+            "url_imagen_usuario": "",
+            "fk_tipo_usuario": 2
+        };
+        this.setState({
+            isLoading: true
+        });
+        const response = await signUp(data);
+        this.setState({
+            isLoading: false
+        });
+        if (response.errors) {
+            response.errors.map((x) => {
+                message.error(x.value + ": " + x.msg);  
+            });
+        }
+        else if (response.message) {
+            message.error(response.message);
+        }
+        else  {
+            message.success("Gracias por registrarte!");
+            setTimeout(() => {
+                const { history } = this.props;
+                history.push("/ingresar");
+            }, 1500);
+        }
+    }
+    
+    render() {
+//         const [form] = Form.useForm();
+        
+        const prefixSelector = (
+            <Form.Item name="prefix" noStyle>
+                <Select
+                style={{
+                    width: 70
+                }}
+                >
+                <Option value="+58">+58</Option>
+                </Select>
+            </Form.Item>
+        );
 
-  const onFinish = (values) => {
-    console.log('Received values of form: ', values);
-  };
+        return (
+            <Form
+                {...formItemLayout}
+//                 form={form}
+                layout='vertical'
+                name="register"
+                onFinish={this.handleSubmit}
+                initialValues={{
+                prefix: '+58'
+                }}
+                scrollToFirstError
+            >
+                <Form.Item
+                name="name"
+                label="Nombre"
+                rules={[
+                    {
+                    required: true,
+                    message: 'Por Favor diganos su Nombre'
+                    }
+                ]}
+                >
+                <Input />
+                </Form.Item>
 
-  const prefixSelector = (
-    <Form.Item name="prefix" noStyle>
-      <Select
-        style={{
-          width: 70
-        }}
-      >
-        <Option value="58">+58</Option>
-        {/*<Option value="87">+87</Option>*/}
-      </Select>
-    </Form.Item>
-  );
-  return (
-    <Form
-      {...formItemLayout}
-      form={form}
-      layout='vertical'
-      name="register"
-      onFinish={onFinish}
-      initialValues={{
-        prefix: '58'
-      }}
-      scrollToFirstError
-    >
-      <Form.Item
-        name="name"
-        label="Nombre"
-        rules={[
-          {
-            required: true,
-            message: 'Por Favor diganos su Nombre'
-          }
-        ]}
-      >
-        <Input />
-      </Form.Item>
+                <Form.Item
+                name="lastname"
+                label="Apellido"
+                rules={[
+                    {
+                    required: true,
+                    message: 'Por Favor diganos su Apellido'
+                    }
+                ]}
+                >
+                <Input />
+                </Form.Item>
 
-      <Form.Item
-        name="lastname"
-        label="Apellido"
-        rules={[
-          {
-            required: true,
-            message: 'Por Favor diganos su Apellido'
-          }
-        ]}
-      >
-        <Input />
-      </Form.Item>
+                <Form.Item
+                name="email"
+                label="Correo Electronico"
+                rules={[
+                    {
+                    type: 'email',
+                    message: 'El correo ingresado no es valido!',
+                    },
+                    {
+                    required: true,
+                    message: 'Por favor ingrese su correo!',
+                    },
+                ]}
+                >
+                <Input />
+                </Form.Item>
 
-      <Form.Item
-        name="email"
-        label="Correo Electronico"
-        rules={[
-          {
-            type: 'email',
-            message: 'El correo ingresado no es valido!',
-          },
-          {
-            required: true,
-            message: 'Por favor ingrese su correo!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+                <Form.Item
+                name="phone"
+                label="Numero de Telefono: "
+                rules={[
+                    {
+                    required: true,
+                    message: 'Por favor ingrese su numero telefonico!',
+                    },
+                ]}
+                >
+                <Input
+                    addonBefore={prefixSelector}
+                    style={{
+                    width: '100%'
+                    }}
+                />
+                </Form.Item>
 
-      <Form.Item
-        name="phone"
-        label="Numero de Telefono: "
-        rules={[
-          {
-            required: true,
-            message: 'Por favor ingrese su numero telefonico!',
-          },
-        ]}
-      >
-        <Input
-          addonBefore={prefixSelector}
-          style={{
-            width: '100%'
-          }}
-        />
-      </Form.Item>
+                <Form.Item
+                name="id"
+                label="cedula de identidad"
+                rules={[
+                    {
+                    required: true,
+                    message: 'Por Favor ingrese su numero de identificacion'
+                    }
+                ]}
+                >
+                <Input />
+                </Form.Item>
 
-      <Form.Item
-        name="id"
-        label="cedula de identidad"
-        rules={[
-          {
-            required: true,
-            message: 'Por Favor ingrese su numero de identificacion'
-          }
-        ]}
-      >
-        <Input />
-      </Form.Item>
+                <Form.Item
+                name="password"
+                label="Contraseña"
+                rules={[
+                    {
+                    required: true,
+                    message: 'Por favor ingrese su contraseña!',
+                    },
+                ]}
+                hasFeedback
+                >
+                <Input.Password />
+                </Form.Item>
 
-      <Form.Item
-        name="password"
-        label="Contraseña"
-        rules={[
-          {
-            required: true,
-            message: 'Por favor ingrese su contraseña!',
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password />
-      </Form.Item>
+                <Form.Item
+                name="confirm"
+                label="Confirmar Contraseña"
+                dependencies={['password']}
+                hasFeedback
+                rules={[
+                    {
+                    required: true,
+                    message: 'Por favor confirme su contraseña!',
+                    },
+                    ({ getFieldValue }) => ({
+                    validator(_, value) {
+                        if (!value || getFieldValue('password') === value) {
+                        return Promise.resolve();
+                        }
 
-      <Form.Item
-        name="confirm"
-        label="Confirmar Contraseña"
-        dependencies={['password']}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: 'Por favor confirme su contraseña!',
-          },
-          ({ getFieldValue }) => ({
-            validator(_, value) {
-              if (!value || getFieldValue('password') === value) {
-                return Promise.resolve();
-              }
+                        return Promise.reject(new Error('Las dos contraseñas no coinciden!'));
+                    },
+                    }),
+                ]}
+                >
+                <Input.Password />
+                </Form.Item>
 
-              return Promise.reject(new Error('Las dos contraseñas no coinciden!'));
-            },
-          }),
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item>
-        <div style={{float:'left'}}>
-          <p>Direccion (Donde quieres recibir los productos)</p>
-        </div>
-      </Form.Item>
-     
-      <Form.Item>
-        <div style={{float:"left", marginBottom:"2%"}}>
-          <AñadirDireccionUSer/>
-        </div>
-      </Form.Item>
-      <Form.Item {...tailFormItemLayout}>
-          <Button type="primary" htmlType="submit">
-            Registrarme
-          </Button>
-      </Form.Item>
-    </Form>
-  );
+                <Form.Item>
+                <div style={{float:'left'}}>
+                    <p>Direccion (Donde quieres recibir los productos)</p>
+                </div>
+                </Form.Item>
+                
+                <Form.Item>
+                <div style={{float:"left", marginBottom:"2%"}}>
+                    <AñadirDireccionUSer/>
+                </div>
+                </Form.Item>
+                <Form.Item {...tailFormItemLayout}>
+                    <Button type="primary" htmlType="submit" loading={this.state.isLoading}>
+                    Registrarme
+                    </Button>
+                </Form.Item>
+            </Form>
+        );
+    }
 };
 
-export default FormRegister;
+export default withRouter(FormRegister);
